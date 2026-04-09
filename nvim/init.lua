026a7a272ab1db95551f6f306442248a6184add3
umrhug <umrhug@gmail.com>
2026-04-09 11:13:55 +0900

    updates

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,26 +1,18 @@
+-- basic settings
 --
 require('base')
-
---
 require('autocmds')
-
--- configure options
 require('options')
-
--- configure keymaps
 require('keymaps')
-
--- configure colorscheme
 require('colorscheme')
---vim.cmd.colorscheme('habamax')
-
--- configure packer
 require('plugins')
 
 -- share clipboard with os
+--
 vim.opt.clipboard:append('unnamedplus,unnamed')
 
 -- call command to read nvim configuration file
+--
 vim.api.nvim_create_user_command(
 	'InitLua',
 	function()
@@ -30,5 +22,6 @@ vim.api.nvim_create_user_command(
 )
 
 -- python3 virtual environment
+--
 vim.g.python3_host_prog = vim.fn.expand('~/.local/share/nvim/venv/bin/python3')
 
